016f5aed13799a04091349f7f6d8b47fb57b68e8
Danny van Kooten <dannyvankooten@gmail.com>
2015-08-20 15:33:17 +0700

    Add `STB.dismiss()` method for dismissing a box.
    This allows you to include the following HTML in a box to close it by clicking.
    
    `
    <a href="javascript: STB.dismiss();">No thanks, I don't want to learn.</a>
    `
    
    Fixes #22

--- assets/js/src/BoxManager.js
+++ assets/js/src/BoxManager.js
@@ -125,18 +125,47 @@ module.exports = (function($) {
 		}
 	}
 
+	// dismiss a single box (or all by omitting id param)
+	function dismiss(id) {
+		// if no id given, dismiss all current open boxes
+		if( typeof(id) === "undefined" ) {
+			dismissAllBoxes();
+		} else if( typeof( boxes[id] ) === "object" ) {
+			boxes[id].dismiss();
+		}
+	}
+
+	function hideBox(id) {
+		if( typeof( boxes[id] ) === "object" ) {
+			boxes[id].hide();
+		}
+	}
+
+	function showBox(id) {
+		if( typeof( boxes[id] ) === "object" ) {
+			boxes[id].show();
+		}
+	}
+
+	function toggleBox(id) {
+		if( typeof( boxes[id] ) === "object" ) {
+			boxes[id].toggle();
+		}
+	}
+
 	// init on document.ready
 	$(document).ready(init);
 
 	// expose a simple API to control all registered boxes
 	return {
 		boxes: boxes,
-		showBox: function(id) { boxes[id].show(); },
-		hideBox: function(id) { boxes[id].hide(); },
-		toggleBox: function(id) { boxes[id].toggle(); },
+		showBox: showBox,
+		hideBox: hideBox,
+		toggleBox: toggleBox,
 		showAllBoxes: showAllBoxes,
 		hideAllBoxes: hideAllBoxes,
 		dismissAllBoxes: dismissAllBoxes,
+		dismiss: dismiss,
 		events: events
 	}
 
