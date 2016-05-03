de631bd7902395b7a268c7ea968fc51a109403a8
Danny van Kooten <dannyvankooten@gmail.com>
2016-05-03 18:02:12 +0200

    Update Boxzilla.js to include exit intent feature. Note: the interface for this will be a premium add-on.

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ var $ = window.jQuery,
         'cookieTime': 0,
         'icon': '&times',
         'minimumScreenWidth': 0,
-        'position': 'bottom-left',
+        'position': 'center',
         'testMode': false,
         'trigger': false,
         'unclosable': false,
@@ -394,7 +394,9 @@ var $ = window.jQuery,
     Box = require('./Box.js')(Boxzilla),
     boxes = {},
     windowHeight = window.innerHeight,
-    overlay = document.createElement('div');
+    overlay = document.createElement('div'),
+    exitIntentDelayTimer,
+    exitIntentTriggered;
 
 function each( obj, callback ) {
     for( var key in obj ) {
@@ -442,7 +444,7 @@ function checkTimeCriteria() {
         if( ! box.mayAutoShow() || box.config.trigger.method !== 'time_on_site' ) {
             return;
         }
-        
+
         if( timeOnSite > box.config.trigger.value ) {
             box.trigger();
         }
@@ -492,6 +494,32 @@ function onOverlayClick(e) {
     });
 }
 
+function triggerExitIntent() {
+    if(exitIntentTriggered) return;
+
+    each(boxes, function(box) {
+        if(box.mayAutoShow() && box.config.trigger.method === 'exit_intent' ) {
+            box.trigger();
+        }
+    });
+
+    exitIntentTriggered = true;
+}
+
+function onMouseLeave(e) {
+    // did mouse leave at top of window?
+    if( e.clientY < 0 ) {
+        exitIntentDelayTimer = window.setTimeout(triggerExitIntent, 1000);
+    }
+}
+
+function onMouseEnter() {
+    if( exitIntentDelayTimer ) {
+        window.clearInterval(exitIntentDelayTimer);
+        exitIntentDelayTimer = null;
+    }
+}
+
 // initialise & add event listeners
 Boxzilla.init = function() {
     // add overlay element to dom
@@ -499,9 +527,11 @@ Boxzilla.init = function() {
     document.body.appendChild(overlay);
 
     // event binds
-    $(window).bind('scroll', throttle(checkHeightCriteria));
-    $(window).bind('resize', throttle(recalculateHeights));
-    $(window).bind('load', recalculateHeights );
+    $(window).on('scroll', throttle(checkHeightCriteria));
+    $(window).on('resize', throttle(recalculateHeights));
+    $(window).on('load', recalculateHeights );
+    $(document).on('mouseleave', onMouseLeave);
+    $(document).on('mouseenter', onMouseEnter);
     $(document).keyup(onKeyUp);
     $(overlay).click(onOverlayClick);
     window.setInterval(checkTimeCriteria, 1000);
