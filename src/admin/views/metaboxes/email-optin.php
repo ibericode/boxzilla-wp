9cab0e81b107169390bb84390d56a6992b088bf6
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-19 11:00:57 +0200

    Improve email optin form in sidebar.

--- src/admin/views/metaboxes/email-optin.php
+++ src/admin/views/metaboxes/email-optin.php
@@ -2,14 +2,15 @@
 defined( 'ABSPATH' ) or exit;
 $user = wp_get_current_user(); ?>
 <form action="//dannyvankooten.us1.list-manage.com/subscribe/post?u=a2d08947dcd3683512ce174c5&amp;id=e3e1e0f8d8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-	<p>Get the most out of this plugin by subscribing to our monthly tips on how to increase your conversion rate.</p>
+	<p>Get the most out of the Boxzilla plugin by subscribing to our newsletter filled with tips & tricks.</p>
+
 	<p class="mc-field-group">
-		<label for="mce-EMAIL">Email Address <span style="color: red;">*</span></label>
+		<label for="mce-EMAIL" style="margin-bottom: 0;">Email Address &nbsp;<span style="color: red;">*</span></label>
 		<input type="email" value="<?php echo esc_attr( $user->user_email ); ?>" name="EMAIL" class="widefat" id="mce-EMAIL">
 	</p>
 	<p class="mc-field-group">
-		<label for="mce-FNAME">First Name </label>
-		<input type="text" value="<?php echo esc_attr( $user->user_firstname ); ?>" name="FNAME" class="widefat" id="mce-FNAME">
+		<label for="mce-FNAME" style="margin-bottom: 0;">First Name &nbsp;<small style="font-weight: normal;">(optional)</small></label>
+		<input type="text" value="<?php echo esc_attr( $user->first_name ); ?>" name="FNAME" class="widefat" id="mce-FNAME">
 	</p>
 	<div id="mce-responses" class="clear">
 		<div class="response" id="mce-error-response" style="display:none"></div>
@@ -18,5 +19,5 @@ $user = wp_get_current_user(); ?>
 	<div style="position: absolute; left: -5000px;"><input type="text" name="b_a2d08947dcd3683512ce174c5_e3e1e0f8d8" tabindex="-1" value=""></div>
 	<div class="clear"><input type="submit" value="Subscribe" name="subscribe" class="button button-primary"></div>
 
-	<p class="help" style="margin-bottom: 0;"><small>No spam, unsubscribe at any time.</small></p>
+	<p class="help" style="margin-bottom: 0;"><small>No spam, unsubscribe at any time, max once a month.</small></p>
 </form>
