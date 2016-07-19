0385bcfc8f5b899a1df5abd82caaa5834e1cd928
Danny van Kooten <dannyvankooten@gmail.com>
2016-07-19 11:00:50 +0200

    Turn "Need help" sidebar block into unordered list.

--- src/admin/views/metaboxes/need-help.php
+++ src/admin/views/metaboxes/need-help.php
@@ -1,3 +1,8 @@
 <?php defined( 'ABSPATH' ) or exit; ?>
-<p>Make sure to look at the <a href="https://kb.boxzillaplugin.com/">online documentation</a>, the <a href="https://wordpress.org/plugins/boxzilla/faq/">frequently asked questions</a> or <a href="https://wordpress.org/support/plugin/boxzilla">use the support forums on WordPress.org</a>.</p>
+<p><?php _e( 'Please make sure to look at the following available resources.', 'boxzilla' ); ?></p>
+<ul class="ul-square">
+    <li><a href="https://kb.boxzillaplugin.com/">Knowledge Base</a></li>
+    <li><a href="https://wordpress.org/plugins/boxzilla/faq/">Frequently Asked Questions</a></li>
+    <li><a href="https://wordpress.org/support/plugin/boxzilla">Support forums on WordPress.org</a></li>
+</ul>
 
