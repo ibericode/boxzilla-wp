843736caeecb90e6d03e12f3780be902e00d5446
Danny van Kooten <dannyvankooten@gmail.com>
2016-08-02 11:07:13 +0200

    Version bump to v3.1.2

--- boxzilla.php
+++ boxzilla.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Boxzilla
-Version: 3.1.1
+Version: 3.1.2
 Plugin URI: https://boxzillaplugin.com/#utm_source=wp-plugin&utm_medium=boxzilla&utm_campaign=plugins-page
 Description: Call-To-Action Boxes that display after visitors scroll down far enough. Unobtrusive, but highly conversing!
 Author: ibericode
@@ -48,7 +48,7 @@ function __load_boxzilla() {
 	}
 
 	define( 'BOXZILLA_FILE', __FILE__ );
-	define( 'BOXZILLA_VERSION', '3.1.1' );
+	define( 'BOXZILLA_VERSION', '3.1.2' );
 
 	require __DIR__ . '/bootstrap.php';
 }
