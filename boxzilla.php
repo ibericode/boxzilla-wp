1b82bf5b3cf7f1c0378d6d9a9063be8c0fe261a5
Danny <dannyvankooten@gmail.com>
2017-08-25 12:22:07 +0200

    add required php version to readme.txt

--- boxzilla.php
+++ boxzilla.php
@@ -11,7 +11,7 @@ Domain Path: /languages/
 License: GPL v3
 
 Boxzilla Plugin
-Copyright (C) 2013-2016, Danny van Kooten, hi@dannyvankooten.com
+Copyright (C) 2013-2017, Danny van Kooten, hi@dannyvankooten.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- readme.txt
+++ readme.txt
@@ -7,6 +7,7 @@ Tested up to: 4.8.1
 Stable tag: 3.1.16
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Requires PHP: 5.3
 
 Flexible call to action boxes, popping up or sliding in at just the right time.
 
