67259c302d75dc0da7d25bbbff60e9e4a8c12b1e
Danny van Kooten <dannyvankooten@gmail.com>
2018-05-31 09:49:27 +0200

    add Jetpack Photon filter

--- src/default-filters.php
+++ src/default-filters.php
@@ -8,3 +8,10 @@ add_filter( 'boxzilla_box_content', 'convert_chars' );
 add_filter( 'boxzilla_box_content', 'wpautop' );
 add_filter( 'boxzilla_box_content', 'shortcode_unautop' );
 add_filter( 'boxzilla_box_content', 'do_shortcode', 11 );
+
+/**
+* Allow Jetpack Photon to filter on Boxzilla box content.
+*/
+if ( class_exists( 'Jetpack' ) && class_exists( 'Jetpack_Photon' ) && Jetpack::is_module_active( 'photon' ) ) {
+   add_filter( 'boxzilla_box_content', array( 'Jetpack_Photon', 'filter_the_content' ), 999999 );
+}
