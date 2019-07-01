483da7bfff714c3837a235518fa86ef31f974a1d
Danny van Kooten <dannyvankooten@gmail.com>
2019-07-01 13:40:44 +0200

    filter nav menu items to use onclick event

--- src/default-filters.php
+++ src/default-filters.php
@@ -15,3 +15,20 @@ add_filter('boxzilla_box_content', 'do_shortcode', 11);
 if (class_exists('Jetpack') && class_exists('Jetpack_Photon') && Jetpack::is_module_active('photon')) {
     add_filter('boxzilla_box_content', array( 'Jetpack_Photon', 'filter_the_content' ), 999999);
 }
+
+/**
+ * Filter nav menu items to use an onclick event instead of a href attribute.
+ *
+ * TODO: Move this to JavaScript instead?
+ */
+add_filter('nav_menu_link_attributes', function ($atts, $item, $args, $depth)
+{
+    if (strpos($atts['href'], '#boxzilla-') !== 0) {
+        return $atts;
+    }
+
+    $id = substr($atts['href'], strlen('#boxzilla-'));
+    $atts['onclick'] = sprintf('Boxzilla.show(%d); return false;', $id);
+    $atts['href'] = '';
+    return $atts;
+}, 10, 4);
