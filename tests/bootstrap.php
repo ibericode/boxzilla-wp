776de713db1d677b3459aa1ab5f1510a44569a5f
Danny van Kooten <hi@dvk.co>
2025-01-06 08:19:28 +0100

    run phpunit in CI

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
 // load autoloader
-require __DIR__ . '/../autoload.php';
-require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/class-wp-test-case.php';
-
-// go!
-echo "Welcome to the Boxzilla PHP Test Suite." . PHP_EOL . PHP_EOL;
+require dirname(__DIR__) . '/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
